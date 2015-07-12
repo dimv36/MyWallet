d4209243c59d1de9a5ffd6227c22636f27cdc410
dimV36 <carriingfate92@yandex.ru>
2015-07-12 14:18:48 +0300

    Statistic Dialog

--- pyMyWallet/modules/dialogs/statisticdialog.py
+++ pyMyWallet/modules/dialogs/statisticdialog.py
@@ -3,15 +3,15 @@ from lxml import etree
 from enum import Enum
 
 from PyQt5.QtWidgets import QDialog, QTreeWidgetItem
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QObject, pyqtSlot, pyqtSignal
 
 from modules.ui.ui_statisticdialog import Ui_StatisticDialog
 
 
 class _StatisticItemType(Enum):
-    UNDEFINED = 0
-    ITEM_YEAR = 1
-    ITEM_MONTH = 2
+    ROOT = QTreeWidgetItem.UserType
+    ITEM_YEAR = QTreeWidgetItem.UserType + 1
+    ITEM_MONTH = QTreeWidgetItem.UserType + 2
 
 
 class _MonthName(Enum):
@@ -32,31 +32,31 @@ class _MonthName(Enum):
         return self.name
 
 
-class _StatisticTableWidgetItem(QTreeWidgetItem):
-    def __init__(self, item_type=_StatisticItemType.UNDEFINED):
-        super().__init__()
-        self.__type = item_type
-
-    def item_type(self):
-        return self.__type
-
-
 class StatisticDialog(QDialog, Ui_StatisticDialog):
     def __init__(self, wallet_root):
         super().__init__()
         self.setupUi(self)
         self.__create_statistic_items(wallet_root)
+        self.__init_signal_slots()
+
+    def __init_signal_slots(self):
+        self._periods.itemClicked.connect(self.__on_item_clicked)
+
+    @pyqtSlot(QTreeWidgetItem)
+    def __on_item_clicked(self, item):
+        print('item clicked: item %s' % (str(item)))
+        print(item.type())
 
     def __create_statistic_items(self, wallet_root):
-        root_item = QTreeWidgetItem(self._periods)
+        root_item = QTreeWidgetItem(self._periods, _StatisticItemType.ROOT.value)
         root_item.setText(0, 'mywallet')
         years = wallet_root.findall('year')
         for year in years:
-            year_item = QTreeWidgetItem(root_item)
+            year_item = QTreeWidgetItem(root_item, _StatisticItemType.ITEM_YEAR.value)
             year_item.setText(0, year.attrib['value'])
             months = year.findall('month')
             for month in months:
-                month_item = QTreeWidgetItem(year_item)
+                month_item = QTreeWidgetItem(year_item, _StatisticItemType.ITEM_MONTH.value)
                 long_month_name = str(_MonthName(int(month.attrib['value'])))
                 month_item.setText(0, QCoreApplication.translate('StatisticDialog', long_month_name))
         self._periods.expandAll()
