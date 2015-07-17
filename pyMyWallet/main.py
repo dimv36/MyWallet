2ab6566840ea1d718047416525f5f173681adc35
Dmitry Voronin <carriingfate92@yandex.ru>
2015-07-17 20:03:08 +0300

    base qt translation

--- pyMyWallet/main.py
+++ pyMyWallet/main.py
@@ -3,7 +3,7 @@ __author__ = 'dimv36'
 
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QTranslator
+from PyQt5.QtCore import QTranslator, QLocale, QLibraryInfo
 from modules.mywallet import MyWallet
 
 
@@ -12,8 +12,11 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     translator = QTranslator()
-    translator.load('ts/pymywallet_RU')
+    base_translator = QTranslator()
+    translator.load('ts/pymywallet_ru')
+    base_translator.load('ts/qtbase_ru')
     app.installTranslator(translator)
+    app.installTranslator(base_translator)
 
     w = MyWallet()
     w.show()
