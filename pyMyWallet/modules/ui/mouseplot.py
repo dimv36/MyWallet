1ec0d0a28a027127d2b5f626a5c9fee43ca0dbd2
dimV36 <carriingfate92@yandex.ru>
2015-07-18 22:08:10 +0300

    mouseplot module

--- pyMyWallet/modules/ui/mouseplot.py
+++ pyMyWallet/modules/ui/mouseplot.py
@@ -1,5 +1,8 @@
 __author__ = 'dimv36'
-from qcustomplot.qcustomplot import QCustomPlot
+from PyQt5.QtGui import QMouseEvent
+from PyQt5.QtCore import Qt
+import qcustomplot
+from qcustomplot.qcustomplot import QCustomPlot, QCPBars, QCPGraph, QCPAbstractItem
 
 class MousePlot(QCustomPlot):
     def __init__(self, parent):
@@ -7,3 +10,24 @@ class MousePlot(QCustomPlot):
 
     def mousePressEvent(self, event):
         print('MousePlot: mousePressEvent')
+        event = QMouseEvent(event)
+        if event.button() == Qt.LeftButton:
+            print('left button clicked')
+            plottable = self.plottableAt(event.pos())
+            if plottable is not None:
+                print('Plottable is not None')
+                x = self.xAxis.pixelToCoord(event.pos().x())
+                y = self.yAxis.pixelToCoord(event.pos().y())
+                bar = self.plottableBarsAt(event.pos())
+                graph = self.plottableGraphAt(event.pos())
+                print(bar)
+                print(graph)
+                if isinstance(plottable, qcustomplot.QCPBars):
+                    print('Found bar')
+                elif isinstance(plottable, qcustomplot.QCPGraph):
+                    print('Found graph')
+                print(x, y)
+            else:
+                print('Plottable is None')
+
+        super().mousePressEvent(event)
