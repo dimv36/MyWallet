3d8ed803a256a27e0867729ddc3d021b99571e76
dimV36 <carriingfate92@yandex.ru>
2015-07-19 12:13:21 +0300

    bars && graph

--- pyMyWallet/modules/ui/mouseplot.py
+++ pyMyWallet/modules/ui/mouseplot.py
@@ -1,8 +1,8 @@
 __author__ = 'dimv36'
 from PyQt5.QtGui import QMouseEvent
-from PyQt5.QtCore import Qt
-import qcustomplot
-from qcustomplot.qcustomplot import QCustomPlot, QCPBars, QCPGraph, QCPAbstractItem
+from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5.QtWidgets import QToolTip
+from qcustomplot.qcustomplot import QCustomPlot
 
 class MousePlot(QCustomPlot):
     def __init__(self, parent):
@@ -16,16 +16,33 @@ class MousePlot(QCustomPlot):
             plottable = self.plottableAt(event.pos())
             if plottable is not None:
                 print('Plottable is not None')
+                need_tooltip = False
                 x = self.xAxis.pixelToCoord(event.pos().x())
                 y = self.yAxis.pixelToCoord(event.pos().y())
                 bar = self.plottableBarsAt(event.pos())
                 graph = self.plottableGraphAt(event.pos())
-                print(bar)
-                print(graph)
-                if isinstance(plottable, qcustomplot.QCPBars):
-                    print('Found bar')
-                elif isinstance(plottable, qcustomplot.QCPGraph):
-                    print('Found graph')
+                if bar:
+                    print('bar found')
+                    need_tooltip = True
+                    data = bar.data()
+                    print(data)
+                elif graph:
+                    print('graph found')
+                    need_tooltip = True
+                if need_tooltip:
+                    QToolTip.showText(event.globalPos(),
+                                      QCoreApplication.translate('MousePlot',
+                                                                 '<table>'
+                                                                 '<tr>'
+                                                                 '<th colspan="2">%s</th>'
+                                                                 '</tr>'
+                                                                 '<tr>'
+                                                                 '<td>X: %s</td>'
+                                                                 '</tr>'
+                                                                 '<tr>'
+                                                                 '<td>Y: %s</td>'
+                                                                 '</tr>'
+                                                                 '</table>' % (plottable.name(), str(x), str(y))))
                 print(x, y)
             else:
                 print('Plottable is None')
