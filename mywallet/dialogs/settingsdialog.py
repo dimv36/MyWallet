140311ff0b530b18d358d9ae0795ca2586933981
Dmitry Voronin <carriingfate92@yandex.ru>
2020-01-01 22:24:41 +0300

    Исправлена проблема в SettingsDialog

--- mywallet/dialogs/settingsdialog.py
+++ mywallet/dialogs/settingsdialog.py
@@ -10,27 +10,27 @@ class SettingsDialog(QDialog, Ui_SettingsDialog):
     def __init__(self, directory, wallet_name=DEFAULT_WALLET_NAME):
         super().__init__()
         self.setupUi(self)
-        self._directory.setText(directory)
-        self._wallet_name.setText(wallet_name)
+        self.directory_edit.setText(directory)
+        self.wallet_edit.setText(wallet_name)
 
         # Подключаем необходимые сигналы к слотам
         self.__init_signal_slots()
 
     def __init_signal_slots(self):
-        self._directory_button.clicked.connect(self.__on_directory_clicked)
+        self.directory_button.clicked.connect(self.__on_directory_clicked)
 
     def directory(self):
-        directory = self._directory.text()
+        directory = self.directory_edit.text()
         if not directory.endswith('/'):
             directory += '/'
         return directory
 
     def wallet_name(self):
-        return self._wallet_name.text()
+        return self.wallet_edit.text()
 
     def __on_directory_clicked(self):
         new_directory = QFileDialog.getExistingDirectory(self,
                                                          self.tr('Open directory'),
                                                          QDir.current().path())
         if new_directory:
-            self._directory.setText(new_directory)
+            self.directory_edit.setText(new_directory)
